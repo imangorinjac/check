3e718420d08cfad8f7070315e19d8a777793d1c2
imangorinjac <iman.gorinjac@nethletic.com>
2021-10-08 17:32:44 +0200

    hi

---
+++ blah.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///registration.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+@app.route("/", methods=["GET", "POST"])
+def fun():
+
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+        var = "mysupersecret"
+        if password == var:
+
+            return render_template("drugi.html", ime=name)
+        else:
+            message = "sorry wrong password"
+            return render_template("mis.html", poruka=message)
+    else:
+
+        return render_template("mis.html")
